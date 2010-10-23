ac450f37621c9a14573c7d133c32fb0efaf87ee3
Brett Viren <brett.viren@gmail.com>
2010-10-23 14:18:53 -0400

    Initial outline of bash setup.

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+install_file () {
+    source=$1 ; shift
+    target=$1 ; shift
+    if [ -z "$source" -o -z "$target" ] ; then 
+	echo "install_file <source> <target>"
+	return
+    fi
+    if [ ! -f $source ] ; then
+	echo "File to install d.n.e.: $source"
+	return
+    fi
+
+    if [ -f $target ] ; then
+	if [ -L $target ] ; then
+	    echo "Removing link: $target"
+	    rm -f $target
+	else
+	    mv $target{,.moved}
+	    echo "Previous $target moved to $target.moved"
+	fi
+    fi
+    echo "Linking $source -> $target"
+    ln -sf $source $target
+}
+
+install_bash () {
+    install_file $HOME/dot/bash/bash_login $HOME/.bash_login
+    install_file $HOME/dot/bash/bash_logout $HOME/.bash_logout
+    install_file $HOME/dot/bash/bashrc $HOME/.bashrc
+    for file in $HOME/.profile $HOME/.bash_profile; do
+	if [ ! -f $file ] ; then continue; fi
+	echo "Moving $file to ${file}.moved"
+	mv $file{,.moved}
+    done
+}
+
+install_bash
