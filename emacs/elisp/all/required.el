5ff58c8dd4c86951383a2fad3cb3a0b241e95938
Brett Viren <brett.viren@gmail.com>
2010-11-07 17:56:14 -0500

    Using snippet mode with editting snippets and add a python 'for' snippet

--- emacs/elisp/all/required.el
+++ emacs/elisp/all/required.el
@@ -6,6 +6,13 @@
 (yas/load-directory "~/.emacs.d/el-get/yasnippet/snippets")
 (yas/load-directory "~/dot/emacs/elisp/yasnippets")
 
+(setq auto-mode-alist (append 
+		       (mapcar 'purecopy
+			       '(
+				 ("/yasnippets/" . snippet-mode)
+				 ("/snippets/" . snippet-mode)
+				 ))))
+
 ;(require 'xcscope)
 ;(require 'pymacs)
 ;(pymacs-load "ropemacs" "rope-")
