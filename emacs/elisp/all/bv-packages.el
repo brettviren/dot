0f737d73f1e4634c44c0e0a3ec44951b81770b46
Brett Viren <bv@bnl.gov>
2010-11-08 12:33:52 -0500

    Get el-get working on RACF by relying on installed-from-source emacs 23.2.
    RHEL/Sci.Linux sucks.

--- emacs/elisp/all/bv-packages.el
+++ emacs/elisp/all/bv-packages.el
@@ -1,9 +1,7 @@
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 (require 'el-get)
 (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get/el-get/recipes")
-(setq el-get-sources
-      ;'(cssh el-get xcscope yasnippet))
-      '(auto-complete xcscope yasnippet))
+(setq el-get-sources '(auto-complete xcscope yasnippet))
 (el-get)
 (provide 'bv-packages)
 
