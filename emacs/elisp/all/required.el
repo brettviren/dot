e14466ccd746890129c83a081064fa1b3139382f
Brett Viren <bv@bnl.gov>
2010-11-11 11:17:34 -0500

    Turn off auto-complete, it is kind of annoying.

--- emacs/elisp/all/required.el
+++ emacs/elisp/all/required.el
@@ -17,11 +17,11 @@
 
 
 ;; auto-complete
-(require 'auto-complete)
-(global-auto-complete-mode t)
-(define-key ac-complete-mode-map "\C-n" 'ac-next)
-(define-key ac-complete-mode-map "\C-p" 'ac-previous)
-(setq ac-auto-start 3)
+;(require 'auto-complete)
+;(global-auto-complete-mode t)
+;(define-key ac-complete-mode-map "\C-n" 'ac-next)
+;(define-key ac-complete-mode-map "\C-p" 'ac-previous)
+;(setq ac-auto-start 3)
 
 ;(require 'xcscope)
 ;(require 'pymacs)
